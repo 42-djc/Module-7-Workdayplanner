b5bd2ac5acf909cd98c75c809982286f4d65fd8b
42-djc <118062286+42-djc@users.noreply.github.com>
2023-03-22 21:34:04 +0000

    added event listener to store timeblock values to local storage

---
+++ starter/script.js
@@ -0,0 +1,13 @@
+// add current date to header
+
+var currentDate = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentDate);
+
+ // ave button click event listener
+ $(".saveBtn").on("click", function() {
+    
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+    console.log(localStorage);
+  });
